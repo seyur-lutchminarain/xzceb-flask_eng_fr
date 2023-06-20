8a3d3b48bc155931ff251a50dad3a87075d2d37f
Seyur Lutchminarain <slutchminarain@oldmutual.com>
2023-06-20 20:48:58 +0200

    ,

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -5,13 +5,13 @@ from translator import french_to_english, english_to_french
 class TestTranslation(unittest.TestCase):
     def test_english_to_french_successful(self):
         english_text = "Hello"
-        expected_french_text = "Pepitoooo"
+        expected_french_text = "Bonjour"
         result = english_to_french(english_text)
         self.assertEqual(result, expected_french_text)
     
     def test_english_to_french_unsuccessful(self):
         english_text = "Hello"
-        expected_french_text = "Bonjour"
+        expected_french_text = "Pepitoo"
         result = english_to_french(english_text)
         self.assertNotEqual(result, expected_french_text)
 
@@ -25,6 +25,6 @@ class TestTranslation(unittest.TestCase):
         french_text = "Bonjour"
         expected_english_text = "Hosh"
         result = french_to_english(french_text)
-        self.assertEqual(result, expected_english_text)
+        self.assertNotEqual(result, expected_english_text)
 
 unittest.main()
